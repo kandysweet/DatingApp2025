b5985f034717bcb45c1ba4b4fe182fbabdc60119
kandysweet <lkandy745@gmail.com>
2025-09-13 01:09:15 -0600

    Parcial01 - Add first entity

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+using System;
+using Microsoft.Net.Http.Headers;
+
+namespace API.Entities;
+
+public class AppUser
+{
+    public required string Id { get; set; }
+    public required string DisplayName { get; set; }
+    public required string Email { get; set; }
+}
