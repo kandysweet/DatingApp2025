ddf1bb0e6da93cafe0376d0ba55abb158ff491a2
kandysweet <lkandy745@gmail.com>
2025-09-14 22:27:34 -0600

    Parcial01 - Add Async code in Members Controller

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -23,7 +23,7 @@ public class MembersController(AppDbContext context) : ControllerBase
     {
         var member = await context.Users.FindAsync(id);
   
-        if (member == null) return NotFound();
+        if (member == null) return NotFound( );
 
         return member;
     }
