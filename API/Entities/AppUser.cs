5830ab75b2ea96d37cd4f248edbb8a392b4da90d
kandysweet <lkandy745@gmail.com>
2025-10-22 00:40:45 -0600

    Parcial02 - Add AccountController and DTO

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,11 +1,10 @@
 using System;
-using Microsoft.Net.Http.Headers;
 
 namespace API.Entities;
 
 public class AppUser
 {
-    public required string Id { get; set; }
+    public string Id { get; set; } = Guid.NewGuid().ToString();
     public required string DisplayName { get; set; }
     public required string Email { get; set; }
 
