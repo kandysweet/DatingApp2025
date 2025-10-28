fc67865ae5f9f042c91d19394ab7e4091cfe2610
kandysweet <lkandy745@gmail.com>
2025-10-27 23:03:04 -0600

    Parcial02 - Add hook to API Register

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -13,8 +13,8 @@ export class AccountService {
   currentUser = signal<User | null>(null);
   baseUrl = "https://localhost:5001/api/";
 
-  register(creds: RegisterCreds){
-    return this.http.post<User>(this.baseUrl+"account/register", creds).pipe(
+  register(creds: RegisterCreds): Observable<User> {
+    return this.http.post<User>(this.baseUrl + "account/login", creds).pipe(
       tap(user => {
         if (user) {
           this.setCurrentUser(user);
