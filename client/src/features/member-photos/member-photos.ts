a69bfe41ada5a2ce2e8d2dd00646277c3e5cf8de
kandysweet <lkandy745@gmail.com>
2025-12-11 01:03:29 -0600

    Parcial03 - Add member detail child components

---
+++ client/src/features/member-photos/member-photos.ts
@@ -0,0 +1,12 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-photos',
+  imports: [],
+  templateUrl: './member-photos.html',
+  styleUrl: './member-photos.css'
+})
+export class MemberPhotos {
+
+}
