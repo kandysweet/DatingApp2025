c149f5c97359f9d00c394b4c090a8e00115af2a5
kandysweet <lkandy745@gmail.com>
2025-12-11 00:23:32 -0600

    Parcial03 - Add memers service

---
+++ client/src/features/test-errors/test-errors.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'app-test-errors',
+@@ -9,44 +10,44 @@
+})
+export class TestErrors {
+  private http = inject(HttpClient);
+  baseUrl = environment.apiUrl;
+  validationErrors = signal<string[]>([]);
+
+  get400ValidationError(): void {
+    this.http.post(this.baseUrl + "account/register", {}).subscribe({
+      next: response => console.log(response),
+      error: error => {
+        console.log(error);
+        this.validationErrors.set(error);
+      }
+    })
+  }
+
+  get400Error(): void {
+    this.http.get(this.baseUrl + "error/bad-request").subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get401Error(): void {
+    this.http.get(this.baseUrl + "error/auth").subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get404Error(): void {
+    this.http.get(this.baseUrl + "error/not-found").subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get500Error(): void {
+    this.http.get(this.baseUrl + "error/server-error").subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+}
