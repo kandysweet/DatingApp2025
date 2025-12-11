7c65bc10da391f1c3834204388a671472510f340
kandysweet <lkandy745@gmail.com>
2025-12-11 01:58:46 -0600

    Parcial03 - Add Photos content

--- client/src/core/services/members-service.ts
+++ client/src/core/services/members-service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Member } from '../../types/member';
+import { Member, Photo } from '../../types/member';
 import { Observable } from 'rxjs';
 
 
@@ -20,12 +20,8 @@ export class MembersService {
   getMembers(): Observable<Member[]> {
     return this.http.get<Member[]>(this.baseUrl + "members");
 
-
-
-
-
-
-
-
+  }
+  getPhotos(id: string) {
+    return this.http.get<Photo[]>(`${this.baseUrl}members/${id}/photos`);
   }
 }
