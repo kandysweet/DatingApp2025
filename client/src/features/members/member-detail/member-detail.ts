838456cb6be7d67115d231ce0ad0779fe8eac214
kandysweet <lkandy745@gmail.com>
2025-12-11 01:52:49 -0600

    Parcial03 - Add Profile content

--- client/src/features/members/member-detail/member-detail.ts
+++ client/src/features/members/member-detail/member-detail.ts
@@ -1,36 +1,18 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { MembersService } from '../../../core/services/members-service';
+
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { AsyncPipe } from '@angular/common';
+
 import { Member } from '../../../types/member';
-import { filter, Observable } from 'rxjs';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-member-detail',
-  imports: [AsyncPipe, RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './member-detail.html',
   styleUrl: './member-detail.css'
 })
 export class MemberDetail implements OnInit {
-  private membersService = inject(MembersService);
+
   private route = inject(ActivatedRoute);
   private router = inject(Router);
- protected member = signal<Member | undefined>(undefined);
-  protected title = signal<string | undefined>("Profile");
-
-  ngOnInit(): void {
-    this.route.data.subscribe({
-        next: data => this.member.set(data["member"])
-    });
-    this.title.set(this.route.firstChild?.snapshot?.title);
-
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe({
-      next: () => {
-        this.title.set(this.route.firstChild?.snapshot?.title);
-      }
-    });
-  }
-
-}
+  protected member = signal<Member | undefined>(undefined);
