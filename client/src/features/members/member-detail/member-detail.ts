dccd483902e2ebbaa8146ab939445142830b9334
kandysweet <lkandy745@gmail.com>
2025-12-11 01:10:30 -0600

    Parcial03 - Add member resolver

--- client/src/features/members/member-detail/member-detail.ts
+++ client/src/features/members/member-detail/member-detail.ts
@@ -15,11 +15,13 @@ export class MemberDetail implements OnInit {
   private membersService = inject(MembersService);
   private route = inject(ActivatedRoute);
   private router = inject(Router);
-  protected member$?: Observable<Member>;
+ protected member = signal<Member | undefined>(undefined);
   protected title = signal<string | undefined>("Profile");
 
   ngOnInit(): void {
-    this.member$ = this.loadMember();
+    this.route.data.subscribe({
+        next: data => this.member.set(data["member"])
+    });
     this.title.set(this.route.firstChild?.snapshot?.title);
 
     this.router.events.pipe(
@@ -31,13 +33,4 @@ export class MemberDetail implements OnInit {
     });
   }
 
-  loadMember(): Observable<Member> | undefined {
-    const id = this.route.snapshot.paramMap.get("id");
-
-    if (id) {
-      return this.membersService.getMember(id);
-    }
-
-    return;
-  }
 }
