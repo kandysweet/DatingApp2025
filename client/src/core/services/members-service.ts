09af629611808c3e76e54fb650b3d4be4f8ad043
kandysweet <118764331+kandysweet@users.noreply.github.com>
2026-03-04 18:21:10 -0600

    Parcial04 - Add edit profile button

---
+++ client/src/core/services/members-service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Member, Photo } from '../../types/member';
+import { Observable } from 'rxjs';
+@@ -10,16 +10,17 @@
+export class MembersService {
+  private http = inject(HttpClient);
+  private baseUrl = environment.apiUrl;
+  editMode = signal(false);
+
+  getMember(id: string): Observable<Member> {
+    return this.http.get<Member>(this.baseUrl + "members/" + id);
+  }
+
+  getMembers(): Observable<Member[]> {
+    return this.http.get<Member[]>(this.baseUrl + "members");
+  }
+
+  getPhotos(id: string) {
+    return this.http.get<Photo[]>(`${this.baseUrl}members/${id}/photos`);
+  }
+}
