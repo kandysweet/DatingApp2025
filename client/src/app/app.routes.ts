e7a2b4eeee5fe2ab69e85b2efff35701973ff0d1
kandysweet <118764331+kandysweet@users.noreply.github.com>
2026-03-04 19:31:06 -0600

    Parcial04 - Add prevent unsave changes guard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,45 @@
 import { Routes } from '@angular/router';
+import { Home } from '../features/home/home';
+import { MemberList } from '../features/members/member-list/member-list';
+import { MemberDetail } from '../features/members/member-detail/member-detail';
+import { Lists } from '../features/lists/lists';
+import { Messages } from '../features/messages/messages';
+import { authGuard } from '../core/guards/auth-guard';
+import { TestErrors } from '../features/test-errors/test-errors';
+import { NotFound } from '../shared/errors/not-found/not-found';
+import { ServerError } from '../shared/errors/server-error/server-error';
+import { MemberProfile } from '../features/member-profile/member-profile';
+import { MemberPhotos } from '../features/member-photos/member-photos';
+import { MemberMessages } from '../features/member-messages/member-messages';
+import { memberResolver } from '../features/members/member-resolver';
+import { preventUnsavedChangesGuard } from '../core/guards/prevent-unsaved-changes-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: Home },
+  {
+    path: "",
+    runGuardsAndResolvers: "always",
+    canActivate: [authGuard],
+    children: [
+      { path: "members", component: MemberList },
+      {
+        path: "members/:id",
+        resolve: { member: memberResolver },
+        runGuardsAndResolvers: "always",
+        component: MemberDetail,
+        children: [
+          { path: "", redirectTo: "profile", pathMatch: "full" },
+          { path: "profile", component: MemberProfile, title: "Profile",
+              canDeactivate: [preventUnsavedChangesGuard] },
+          { path: "photos", component: MemberPhotos, title: "Photos" },
+          { path: "messages", component: MemberMessages, title: "Messages" },
+        ]
+      },
+      { path: "lists", component: Lists },
+      { path: "messages", component: Messages }
+    ]
+  },
+  { path: "errors", component: TestErrors },
+  { path: "server-error", component: ServerError },
+  { path: "**", component: NotFound }
+];
