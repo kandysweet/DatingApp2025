09f37e0c625ba79a19f5eaf8cad47e4ef23a1038
kandysweet <118764331+kandysweet@users.noreply.github.com>
2026-03-04 21:05:26 -0600

    Parcial04 - Add main photo selection

--- client/src/features/member-photos/member-photos.ts
+++ client/src/features/member-photos/member-photos.ts
@@ -1,19 +1,18 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { MembersService } from '../../core/services/members-service';
 import { ActivatedRoute } from '@angular/router';
-import { Photo } from '../../types/member';
+import { Member, Photo } from '../../types/member';
 import { ImageUpload } from "../../shared/image-upload/image-upload";
-
+import { AccountService } from '../../core/services/account-service';
+import { User } from '../../types/user';
 
 @Component({
   selector: 'app-member-photos',
-  imports: [ImageUpload],
-  templateUrl: './member-photos.html',
-  styleUrl: './member-photos.css'
+@@ -12,37 +14,52 @@
 })
 export class MemberPhotos implements OnInit {
-
   private route = inject(ActivatedRoute);
+  private accountService = inject(AccountService);
   protected membersService = inject(MembersService);
   protected photos = signal<Photo[]>([]);
   protected loading = signal(false);
@@ -46,5 +45,19 @@ export class MemberPhotos implements OnInit {
         this.loading.set(false);
       }
     })
+  }
+
+  setMainPhoto(photo: Photo) {
+    this.membersService.setMainPhoto(photo).subscribe({
+      next: () => {
+        const currentUser = this.accountService.currentUser();
+        if (currentUser) currentUser.imageUrl = photo.url;
+        this.accountService.setCurrentUser(currentUser as User);
+        this.membersService.member.update(member => ({
+          ...member,
+          imageUrl: photo.url
+        }) as Member);
+      }
+    });
   }
 }
