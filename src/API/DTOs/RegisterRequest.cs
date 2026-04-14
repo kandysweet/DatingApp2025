649376037fc4a06bd14a188d1bcbda6a028b7a1e
kandysweet <118764331+kandysweet@users.noreply.github.com>
2026-04-13 20:36:30 -0600

    Parcial05 - Updater account register

---
+++ src/API/DTOs/RegisterRequest.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class RegisterRequest
+{
+    [Required]
+    public string DisplayName { get; set; } = string.Empty;
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    [Required]
+    [MinLength(6)]
+    public string Password { get; set; } = string.Empty;
+
+    [Required] public string Gender { get; set; } = string.Empty;
+    [Required] public string City { get; set; } = string.Empty;
+    [Required] public string Country { get; set; } = string.Empty;
+    [Required] public DateOnly BirthDay { get; set; }
+}
