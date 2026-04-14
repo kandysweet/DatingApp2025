e285ef8c196c726185f2b4e10ac99b2f4ec5cbe2
kandysweet <118764331+kandysweet@users.noreply.github.com>
2026-04-13 20:03:49 -0600

    Parcial05 - Add reactive register form

---
+++ client/src/features/account/register/register.ts
@@ -0,0 +1,46 @@
+import { Component, inject, OnInit, output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AccountService } from '../../../core/services/account-service';
+import { RegisterCreds } from '../../../types/user';
+import { JsonPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-register',
+  imports: [ReactiveFormsModule, JsonPipe],
+  templateUrl: './register.html',
+  styleUrl: './register.css'
+})
+export class Register implements OnInit {
+  private accountService = inject(AccountService);
+
+  protected creds = {} as RegisterCreds;
+  protected registerForm: FormGroup = new FormGroup({});
+  cancelRegister = output<boolean>();
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      email: new FormControl(),
+      displayName: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
+    })
+  }
+
+  register(): void {
+    console.group("REGISTER");
+    console.log(this.registerForm.value);
+    console.groupEnd();
+    // this.accountService.register(this.creds).subscribe({
+    //   next: response => {
+    //     console.log(response);
+    //     this.cancel();
+    //   },
+    //   error: error => console.log(error)
+    // });
+  }
+
+  cancel(): void {
