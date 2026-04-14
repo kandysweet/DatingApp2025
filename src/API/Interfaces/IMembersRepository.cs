dfa7d8c392ef2ec0f32ad7a11a17761bd54308fe
kandysweet <118764331+kandysweet@users.noreply.github.com>
2026-04-13 21:40:54 -0600

    Parcial05 - Add pagination to BE

---
+++ src/API/Interfaces/IMembersRepository.cs
@@ -0,0 +1,13 @@
+using API.Entities;
+using API.Helpers;
+
+namespace API.Interfaces;
+
+public interface IMembersRepository
+{
+    void Update(Member member);
+    Task<bool> SaveAllAsync();
+    Task<PaginationResult<Member>> GetMembersAsync(PaginationRequest paginationRequest);
+    Task<Member?> GetMemberAsync(string id);
+    Task<IReadOnlyList<Photo>> GetPhotosAsync(string memberId);
+    Task<Member?> GetMemberForUpdateAsync(string id);
