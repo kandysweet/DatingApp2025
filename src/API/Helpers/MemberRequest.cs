2f759f47add3e21dff8482ea295681ca00eda84d
kandysweet <118764331+kandysweet@users.noreply.github.com>
2026-04-16 18:40:06 -0600

    Pacial05 - Add more filtering to API

--- src/API/Helpers/MemberRequest.cs
+++ src/API/Helpers/MemberRequest.cs
@@ -1,8 +1,9 @@
-
 namespace API.Helpers;
 
 public class MemberRequest : PaginationRequest
 {
     public string? Gender { get; set; }
     public string? CurrentMemberId { get; set; }
+    public int MinAge { get; set; } = 18;
+    public int MaxAge { get; set; } = 120;
 }
