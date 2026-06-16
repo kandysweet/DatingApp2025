f6c173ebc22b1d5d5207c727091b727b299d76f5
kandysweet <118764331+kandysweet@users.noreply.github.com>
2026-06-16 17:40:03 -0600

    Parcial06 - Add initial migration for Message

--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -1,9 +1,67 @@
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Data;
 
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<AppUser> Users { get; set; }
+    public DbSet<Member> Members { get; set; }
+    public DbSet<Photo> Photos { get; set; }
+    public DbSet<MemberLike> Likes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<MemberLike>()
+            .HasKey(m => new { m.SourceMemberId, m.TargetMemberId });
+
+        modelBuilder.Entity<MemberLike>()
+            .HasOne(s => s.SourceMember)
+            .WithMany(t => t.LikedMembers)
+            .HasForeignKey(s => s.SourceMemberId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<MemberLike>()
+            .HasOne(s => s.TargetMember)
+            .WithMany(t => t.LikedByMembers)
+            .HasForeignKey(s => s.TargetMemberId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Entity<Message>()
+            .HasOne(m => m.Recipient)
+            .WithMany(mr => mr.MessagesReceived)
+            .OnDelete(DeleteBehavior.Restrict);
+        
+        modelBuilder.Entity<Message>()
+            .HasOne(m => m.Sender)
+            .WithMany(mr => mr.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // modelBuilder.Entity<Message>()
+        //     .Property(m => m.RecipientDeleted)
+        //     .HasDefaultValue(false);
+        
+        // modelBuilder.Entity<Message>()
+        //     .Property(m => m.SenderDeleted)
+        //     .HasDefaultValue(false);
+
+        var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
+            v => v.ToUniversalTime(),
+            v => DateTime.SpecifyKind(v, DateTimeKind.Utc)
+        );
+
+        foreach(var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            foreach(var property in entityType.GetProperties())
+            {
+                if (property.ClrType == typeof(DateTime))
+                {
+                    property.SetValueConverter(dateTimeConverter);
+                }
+            }
+        }
+    }
 }
