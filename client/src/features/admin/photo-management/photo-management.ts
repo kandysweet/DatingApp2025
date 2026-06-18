565faf1ba3f77d0dc893ca49591b43a30fa44e85
kandysweet <118764331+kandysweet@users.noreply.github.com>
2026-06-17 22:43:00 -0600

    Parcial06 - IDENTITY - Add edit roles component

---
+++ client/src/features/admin/photo-management/photo-management.ts
@@ -0,0 +1,12 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-management',
+  imports: [],
+  templateUrl: './photo-management.html',
+  styleUrl: './photo-management.css'
+})
+export class PhotoManagement {
+
+}
