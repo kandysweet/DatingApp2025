3fabb10751d7d190e4766c4fe453ab6d0876a5c7
kandysweet <118764331+kandysweet@users.noreply.github.com>
2026-06-19 13:23:42 -0600

    Parcial06 - SignalR - Add traceability of message groups

--- src/API/Data/MessagesRepository.cs
+++ src/API/Data/MessagesRepository.cs
@@ -10,12 +10,15 @@ namespace API.Data;
 public class MessagesRepository(AppDbContext context) : IMessagesRepository
 {
     public void Add(Message message) => context.Messages.Add(message);
+    public void AddGroup(Group group) => context.Groups.Add(group);
 
     public void Delete(Message message) => context.Messages.Remove(message);
 
-    public async Task<Message?> Get(string messageId) => await context.Messages.FindAsync(messageId);
+    public async Task<Message?> GetAsync(string messageId) => await context.Messages.FindAsync(messageId);
 
-     public async Task<PaginationResult<MessageResponse>> GetForMember(MessageParams messageParams)
+    public async Task<Connection?> GetConnectionAsync(string connectionId) => await context.Connections.FindAsync(connectionId);
+
+     public async Task<PaginationResult<MessageResponse>> GetForMemberAsync(MessageParams messageParams)
     {
         var query = context.Messages
             .OrderByDescending(m => m.MessageSent)
@@ -32,7 +35,18 @@ public class MessagesRepository(AppDbContext context) : IMessagesRepository
         return await Pagination.CreateAsync(messageQuery, messageParams.PageNumber, messageParams.PageSize);
     }
 
-    public async Task<IReadOnlyList<MessageResponse>> GetThread(string currentMemberId, string recipientId)
+    public async Task<Group?> GetGroupForConnectionAsync(string connectionId)
+        => await context.Groups
+            .Include(g => g.Connections)
+            .Where(g => g.Connections.Any(c => c.ConnectionId == connectionId))
+            .FirstOrDefaultAsync();
+
+    public async Task<Group?> GetMessageGroupAsync(string groupName)
+        => await context.Groups
+            .Include(g => g.Connections)
+            .FirstOrDefaultAsync(g => g.Name == groupName);
+
+    public async Task<IReadOnlyList<MessageResponse>> GetThreadAsync(string currentMemberId, string recipientId)
     {
           await context.Messages
             .Where(m => m.RecipientId == currentMemberId
@@ -48,6 +62,11 @@ public class MessagesRepository(AppDbContext context) : IMessagesRepository
             .Select(MessageMapper.ToResponseProjection())
             .ToListAsync();
     }
+
+    public async Task RemoveConnectionAsync(string connectionId)
+        => await context.Connections
+            .Where(c => c.ConnectionId == connectionId)
+            .ExecuteDeleteAsync();
 
     public async Task<bool> SaveAllAsync() => await context.SaveChangesAsync() > 0;
 }
